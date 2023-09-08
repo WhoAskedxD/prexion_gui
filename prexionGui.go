ccb9661f00d430b4f010a79e4cc2d55c06d6b24e
WhoAskedxD <harryvan667@gmail.com>
2023-09-08 16:31:58 -0700

    running new test

--- prexionGui.go
+++ prexionGui.go
@@ -9,13 +9,16 @@ import (
 
 func main() {
 
-	//folderPath := "/Users/harrymbp/Developer/Projects/PreXion/temp"
-	dicomFilePath := "/Users/harrymbp/Developer/Projects/PreXion/temp/1.2.392.200036.9163.41.127414021.344261765/1.2.392.200036.9163.41.127414021.344261765.10632.1/00000_1.2.392.200036.9163.41.127414021.344261765.10632.2.dcm"
-	results, err := anonymize_scans.DicomInfoGrabber(dicomFilePath)
+	folderPath := "/Users/harrymbp/Developer/Projects/PreXion/temp"
+	//dicomFilePath := "/Users/harrymbp/Developer/Projects/PreXion/dicomFiles/2313920.1194420868.1125777922.144317013718248927734763.2419061/2313920.1194420868.1125777922.206729801520575063.241906.824.11/00001_2313920.1194420868.1125777922.206415228720675263.241906.824.21.dcm"
+	results, err := anonymize_scans.GetFilePathsInFolders(folderPath)
 	if err != nil {
 		log.Println("ERROR:", err)
+		fmt.Println("not a valid dicom file.")
 		return
 	}
-	fmt.Println(results)
+	for _, file := range results {
+		anonymize_scans.GetDicomFolders(folderPath, file)
+	}
 
 }
