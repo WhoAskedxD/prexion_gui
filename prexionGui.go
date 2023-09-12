1f94ffde578ae0acef357f6e723a43409e12ce21
WhoAskedxD <harryvan667@gmail.com>
2023-09-12 16:55:18 -0700

    added timmer and testing

--- prexionGui.go
+++ prexionGui.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/WhoAskedxD/anonymize_scans"
 )
 
 func main() {
-
+	startTime := time.Now()
 	folderPath := "/Users/harrymbp/Developer/Projects/PreXion/temp"
-	searchPath := "/Users/harrymbp/Developer/Projects/PreXion/temp/1.2.392.200036.9163.41.127414021.344261765"
-	//dicomFilePath := "/Users/harrymbp/Developer/Projects/PreXion/dicomFiles/2313920.1194420868.1125777922.144317013718248927734763.2419061/2313920.1194420868.1125777922.206729801520575063.241906.824.11/00001_2313920.1194420868.1125777922.206415228720675263.241906.824.21.dcm"
-	// results, err := anonymize_scans.GetFilePathsInFolders(folderPath)
-	// if err != nil {
-	// 	log.Println("ERROR:", err)
-	// 	fmt.Println("not a valid dicom file.")
-	// 	return
-	// }
-	testPath := "/Volumes/Harrypc/temp/PX4Scan/20230817065427_1_Ceph/40920587/45724265/00001DCM.dcm"
-	anonymize_scans.GetDicomFolders(folderPath, searchPath)
-	anonymize_scans.CheckScanType(testPath)
+	dicomFolders, err := anonymize_scans.GetDicomFolders(folderPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("dicom Folders are ", dicomFolders)
+	endTime := time.Now()
+	elapsedTime := endTime.Sub(startTime)
+	fmt.Printf("Elapsed time: %.2f seconds\n", elapsedTime.Seconds())
 }
