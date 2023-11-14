09bec0ab4fb6b67693e4cf0eeb95b74b1b681bdf
WhoAskedxD <harryvan667@gmail.com>
2023-11-13 16:35:03 -0800

    need to make list of default anonymize values and tags

--- prexionGui.go
+++ prexionGui.go
@@ -6,21 +6,43 @@ import (
 	"time"
 
 	"github.com/WhoAskedxD/anonymize_scans"
+	"github.com/suyashkumar/dicom/pkg/tag"
 )
 
 func main() {
 	startTime := time.Now()
 	folderPath := "/Users/harrymbp/Developer/Projects/PreXion/temp"
+	outputPath := "/Users/harrymbp/Developer/Projects/PreXion/output"
 	dicomFolders, err := anonymize_scans.GetDicomFolders(folderPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	dicomReference, err := anonymize_scans.GetScanNames(dicomFolders)
-	for key, value := range dicomReference {
-		fmt.Printf("parent folder is %s, while scan is %s\n", key, value)
+	// listOfScans, err := anonymize_scans.GetScanNames(dicomFolders)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//data to modify the dicom info with. takes in tag type for key and value string. | https://pkg.go.dev/github.com/suyashkumar/dicom@v1.0.7/pkg/tag#pkg-constants
+	newDicomAttribute := map[tag.Tag]string{
+		tag.PatientName: "new testing function",
 	}
-	if err != nil {
-		log.Fatal(err)
+	//need to make a function to let users select which Tags they want to modify.
+
+	counter := 0
+	for key, value := range dicomFolders {
+		fmt.Printf("current key is: %s\nValue: %s\n", key, value)
+		folderInfo, err := anonymize_scans.MakeOutputPath(key, outputPath, counter, value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("outputPaths for:%s\nare %s\n", key, folderInfo)
+		fmt.Printf("sending folderInfo to MakeDicomFolder")
+		err = anonymize_scans.MakeDicomFolders(folderInfo, newDicomAttribute)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("done making Dicoms")
+		counter++
+		break
 	}
 	endTime := time.Now()
 	elapsedTime := endTime.Sub(startTime)
