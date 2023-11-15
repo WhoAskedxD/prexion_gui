9641b333c38d330552827a60a99053de225ee231
WhoAskedxD <harryvan667@gmail.com>
2023-11-14 16:53:16 -0800

    added more tags and varibles to test

--- prexionGui.go
+++ prexionGui.go
@@ -9,10 +9,17 @@ import (
 	"github.com/suyashkumar/dicom/pkg/tag"
 )
 
+//Notes
+//UID instances
+//Syntax for a UID is RootUID + Unit UID + Timestamp 17 digits long + SUBUID
+//example 1.2.392.200036.9163.31.0938.20231019103353867.1.1
+//ROOTUID = 1.2.392.200036.9163 | Unit UID = 31 | Timestamp = 20231019103353867 || SUBUID = 1.1
+
 func main() {
 	startTime := time.Now()
-	folderPath := "/Users/harrymbp/Developer/Projects/PreXion/temp"
+	folderPath := "/Users/harrymbp/Developer/Projects/PreXion/temp copy"
 	outputPath := "/Users/harrymbp/Developer/Projects/PreXion/output"
+	//grab parentFolder and scan details related to that parent folder
 	dicomFolders, err := anonymize_scans.GetDicomFolders(folderPath)
 	if err != nil {
 		log.Fatal(err)
@@ -21,6 +28,7 @@ func main() {
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
+
 	//data to modify the dicom info with. takes in tag type for key and value string. | https://pkg.go.dev/github.com/suyashkumar/dicom@v1.0.7/pkg/tag#pkg-constants
 	newDicomAttribute := map[tag.Tag]string{
 		tag.PatientName: "new testing function",
@@ -28,13 +36,14 @@ func main() {
 	//need to make a function to let users select which Tags they want to modify.
 
 	counter := 0
-	for key, value := range dicomFolders {
-		fmt.Printf("current key is: %s\nValue: %s\n", key, value)
-		folderInfo, err := anonymize_scans.MakeOutputPath(key, outputPath, counter, value)
+	for parentPath, scanDetails := range dicomFolders {
+		fmt.Printf("current key is: %s\nValue: %s\n", parentPath, scanDetails)
+		//generate a map that associates each scan with a new output path
+		folderInfo, err := anonymize_scans.MakeOutputPath(parentPath, outputPath, counter, scanDetails)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("outputPaths for:%s\nare %s\n", key, folderInfo)
+		fmt.Printf("outputPaths for:%s\nare %s\n", parentPath, folderInfo)
 		fmt.Printf("sending folderInfo to MakeDicomFolder")
 		err = anonymize_scans.MakeDicomFolders(folderInfo, newDicomAttribute)
 		if err != nil {
@@ -42,6 +51,12 @@ func main() {
 		}
 		fmt.Printf("done making Dicoms")
 		counter++
+		//test code block
+		// listOfScans, err := anonymize_scans.GetScanList(scanDetails)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// fmt.Print(listOfScans)
 		break
 	}
 	endTime := time.Now()
