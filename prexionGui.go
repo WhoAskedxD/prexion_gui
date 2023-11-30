6155716277b1a264362c337eaff95e8bb0321459
WhoAskedxD <harryvan667@gmail.com>
2023-11-29 16:56:09 -0800

    anonymize is working

--- prexionGui.go
+++ prexionGui.go
@@ -13,51 +13,56 @@ import (
 //UID instances
 //Syntax for a UID is RootUID + Unit UID + Timestamp 17 digits long + SUBUID
 //example 1.2.392.200036.9163.31.0938.20231019103353867.1.1
-//ROOTUID = 1.2.392.200036.9163 | Unit UID = 31 | Timestamp = 20231019103353867 || SUBUID = 1.1
+//ROOTUID = 1.2.392.200036.9163 | Unit UID = 31.0938 | Timestamp = 20231019103353867 || SUBUID = 1.1
 
 func main() {
 	startTime := time.Now()
-	folderPath := "/Users/harrymbp/Developer/Projects/PreXion/temp copy"
-	outputPath := "/Users/harrymbp/Developer/Projects/PreXion/output"
+	folderPath := "/Users/harrymbp/Developer/Projects/PreXion/temp/"
+	outputPath := "/Volumes/Harrypc/temp/Anonymized scans"
 	//grab parentFolder and scan details related to that parent folder
 	dicomFolders, err := anonymize_scans.GetDicomFolders(folderPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// listOfScans, err := anonymize_scans.GetScanNames(dicomFolders)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	//data to modify the dicom info with. takes in tag type for key and value string. | https://pkg.go.dev/github.com/suyashkumar/dicom@v1.0.7/pkg/tag#pkg-constants
-	newDicomAttribute := map[tag.Tag]string{
-		tag.PatientName: "new testing function",
-	}
+	// newDicomAttribute := map[tag.Tag]string{
+	// 	tag.PatientName: "new testing function",
+	// 	tag.PatientID:   "1700079492",
+	// }
 	//need to make a function to let users select which Tags they want to modify.
-
+	//create a rootUID and UnitUID = 1.2.392.200036.9163.99.9999
+	totalscans := len(dicomFolders)
+	fmt.Printf("found %d scans\n", totalscans)
 	counter := 0
 	for parentPath, scanDetails := range dicomFolders {
-		fmt.Printf("current key is: %s\nValue: %s\n", parentPath, scanDetails)
+		fmt.Printf("started working on scan: %s\n", parentPath)
 		//generate a map that associates each scan with a new output path
+		fmt.Printf("Generating output paths\n")
 		folderInfo, err := anonymize_scans.MakeOutputPath(parentPath, outputPath, counter, scanDetails)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("outputPaths for:%s\nare %s\n", parentPath, folderInfo)
-		fmt.Printf("sending folderInfo to MakeDicomFolder")
-		err = anonymize_scans.MakeDicomFolders(folderInfo, newDicomAttribute)
+		fmt.Printf("outputPaths are %s\n", folderInfo)
+		fmt.Printf("Generating new information for the dicom files\n")
+		//take rootid and time stamp and add them together to create a studyUID for the parent folder and add it to the current newPatientInfo
+		newPatientInfo, err := anonymize_scans.RandomizePatientInfo(scanDetails)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("done making Dicoms")
+		//set the tag info for StudyInstanceuid to a value we will modify this when working with the scans.
+		newPatientInfo[tag.StudyInstanceUID] = "1.2.392.200036.9163.99.9999"
+		fmt.Printf("Finished generating new dicom info\n")
+		logResults, _ := anonymize_scans.LogAnonymizedScan(scanDetails, newPatientInfo)
+		fmt.Printf("Generating new dicoms\n")
+		err = anonymize_scans.MakeDicomFolders(folderInfo, newPatientInfo)
+		if err != nil {
+			log.Fatal(err)
+		}
 		counter++
 		//test code block
-		// listOfScans, err := anonymize_scans.GetScanList(scanDetails)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// fmt.Print(listOfScans)
-		break
+		fmt.Printf("finished anonymizing Dicoms for %s and ORGINIALPATIENTID is:%s, NEWPATIENTID is:%s\n", logResults["LOCATION"], logResults["ORGINIALPATIENTID"], logResults["NEWPATIENTID"])
+		fmt.Printf("finished working on scan %d out of %d\n\n", counter, totalscans)
+		// break
 	}
 	endTime := time.Now()
 	elapsedTime := endTime.Sub(startTime)
